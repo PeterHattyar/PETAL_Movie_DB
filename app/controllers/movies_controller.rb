8a697bbfab8afd02f0aeb8c2f0e1305c2584ff3d
PeterHattyar <peter.hattyar@digital.cabinet-office.gov.uk>
2025-02-03 04:28:35 +0000

    Created Movie model, database with 1 table, and 4 columns, 2 strings, 1 date, 1 text. Title, Director, Release and Genre.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,40 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+  
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
   def new
+    @movie = Movie.new
+  end
+
+  def create
+    # Initial, bumps into rails security error requiring strong params!
+    # @movie = Movie.new(params[:movie])
+  
+    # This is now corretly allows our params, but it is preferred to be factored out.
+    # @movie = Movie.new(params.require(:movie).permit(:title, :director, :release, :genre))
+
+    @movie = Movie.new(movie_params)
+
+    # Previous state, without validation
+    # @movie.save
+    # redirect_to @movie
+
+    # New state, with validation in place.
+    # Note the if condition is enough for @movie.save to be called.
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :director, :release, :genre)
   end
 end
